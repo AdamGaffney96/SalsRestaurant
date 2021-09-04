88036f12b758c76a9bccca307e5402628e32b142
AdamGaffney96 <AdamGaffney96@gmail.com>
2021-09-04 01:02:47 +0100

    Various styling changes, implementation of a mobile interface and menus

--- SalsRestaurant/static/restaurant/js/main.js
+++ SalsRestaurant/static/restaurant/js/main.js
@@ -7,25 +7,27 @@ const bot = document.querySelector('.bot');
 const navMenu = document.querySelector('.nav-menu');
 const viewMenu = document.querySelector('.view-menu-link');
 const body = document.body;
+const navItems = document.querySelectorAll('.nav-item');
+console.log(navItems)
+navItems.forEach((e) => e.firstElementChild.setAttribute("tabindex", "-1"));
 
 ham.addEventListener("click", (e) => {
     if (topLine.style.transform) {
         topLine.style.transform = null;
         mid.style.transform = null;
         bot.style.transform = null;
-        navMenu.style.display = null;
-        navMenu.style.transform = null;
+        navMenu.style.height = null;
         body.style.overflowY = null;
         viewMenu.removeAttribute("tabindex");
-
+        navItems.forEach((e) => e.firstElementChild.setAttribute("tabindex", "-1"));
     } else {
         topLine.style.transform = "rotate(-45deg)";
         bot.style.transform = "rotate(45deg)";
         mid.style.transform = "translateX(200%)";
-        navMenu.style.display = "block";
-        navMenu.style.transform = "translateY(0)";
+        navMenu.style.height = "100%";
         body.style.overflowY = "hidden";
         viewMenu.setAttribute("tabindex", "-1");
+        navItems.forEach((e) => e.firstElementChild.removeAttribute("tabindex"));
     }
 })
 
@@ -35,17 +37,17 @@ ham.addEventListener("keydown", (e) => {
         topLine.style.transform = null;
         mid.style.transform = null;
         bot.style.transform = null;
-        navMenu.style.display = null;
-        navMenu.style.transform = null;
+        navMenu.style.height = null;
         body.style.overflowY = null;
         viewMenu.removeAttribute("tabindex");
+        navItems.forEach((e) => e.firstElementChild.setAttribute("tabindex", "-1"));
     } else {
         topLine.style.transform = "rotate(-45deg)";
         bot.style.transform = "rotate(45deg)";
         mid.style.transform = "translateX(200%)";
-        navMenu.style.display = "block";
-        navMenu.style.transform = "translateY(0)";
+        navMenu.style.height = "100%";
         body.style.overflowY = "hidden";
         viewMenu.setAttribute("tabindex", "-1");
+        navItems.forEach((e) => e.firstElementChild.removeAttribute("tabindex"));
     }
 })
